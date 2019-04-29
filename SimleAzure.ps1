e17b555ef12e27ba7c03a5274e08d1e38c8c25af
Manikandan V <35761302+ManiVensi@users.noreply.github.com>
2019-04-29 15:36:56 +0530

    Add files via upload

---
+++ SimleAzure.ps1
@@ -0,0 +1,26 @@
+﻿import AzureRMAccount 
+Add-AzureRmAccount -Subscription "AZU-P00-RA01-NP" 
+
+$resourceGroup = "AZ19D1-ZeroTouch"
+$location = "West US 2"
+$vmName = "Testing-Mani"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create a resource group
+#New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+# Create a virtual machine
+New-AzureRmVM `
+  -ResourceGroupName $resourceGroup `
+  -Name $vmName `
+  -Location $location `
+  -ImageName "Win2016Datacenter" `
+  -VirtualNetworkName "myVnet" `
+  -SubnetName "mySubnet" `
+  -SecurityGroupName "myNetworkSecurityGroup" `
+  -PublicIpAddressName "myPublicIp" `
+  -Credential $cred `
+  -OpenPorts 3389
+  
